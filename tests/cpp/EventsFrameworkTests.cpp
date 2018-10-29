8f8b4f58af4846f49e38999b02298e864a97b606
Luiz Silveira <zertyz@gmail.com>
2018-10-29 20:31:36 -0300

    better templates -- yet to allow variants

--- tests/cpp/EventsFrameworkTests.cpp
+++ tests/cpp/EventsFrameworkTests.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 #include <functional>
 using namespace std;
 
@@ -11,6 +12,86 @@ using namespace std;
 #include <BetterExceptions.h>
 #include <TimeMeasurements.h>
 using namespace mutua::cpputils;
+
+
+
+struct EventLinkSuiteObjects {
+
+    // test case constants
+    //static constexpr unsigned int numberOfCalls  = 4'096'000*512;
+
+    // test case data
+    //static   std::vector  <std::string>* stdStringKeys;		// keys for all by EASTL containers
+
+    // output messages for boost tests
+    static string testOutput;
+
+
+    EventLinkSuiteObjects() {
+    	static bool firstRun = true;
+    	if (firstRun) {
+			cerr << endl << endl;
+			cerr << "Event Link Tests:" << endl;
+			cerr << "================ " << endl << endl;
+			firstRun = false;
+    	}
+    }
+    ~EventLinkSuiteObjects() {
+    	BOOST_TEST_MESSAGE("\n" + testOutput);
+    	testOutput = "";
+    }
+
+    static void output(string msg, bool toCerr) {
+    	if (toCerr) cerr << msg << flush;
+    	testOutput.append(msg);
+    }
+    static void output(string msg) {
+    	output(msg, true);
+    }
+
+};
+// static initializers
+string EventLinkSuiteObjects::testOutput = "";
+
+BOOST_FIXTURE_TEST_SUITE(EventLinkSuiteSuite, EventLinkSuiteObjects);
+
+template<typename _Argument>
+struct EventDefinition {
+	void(&&procedureReference)(_Argument);
+	EventDefinition(void(&&procedureReference)(_Argument))
+		: procedureReference(procedureReference) {}
+};
+
+enum class MyEvents {EventA, EventB, EventC, last=EventC};
+template <typename _Argument, class E, class = std::enable_if_t<std::is_enum<E>{}>>
+class EventFramework {
+public:
+	EventFramework(const EventDefinition<_Argument> (&eventHandlers)[((int)(E::last)) + 1]) {
+		int eventsEnumerationLength = ((int)(E::last)) + 1;
+		cerr << "Now at EventFramework!" << endl;
+		cerr << "length of E:   " << eventsEnumerationLength << endl;
+		cerr << "events.size(): " << sizeof(eventHandlers) / sizeof(eventHandlers[0]) << endl;
+//		for (int i=0; i<events.size(); i++) {
+//			int v = (int)static_cast<E>(static_cast<std::underlying_type_t<E>>(events[i]));
+//			cerr << "events[" << i << "] = '" << v << "'" << endl;
+//		}
+	}
+};
+//E &operator ++ (E &e) {
+//    return e = static_cast<E>(static_cast<std::underlying_type_t<E>>(e) + 1);
+//}
+
+void _myShits(int p)    { cerr << "my  int   shits with p is " << p << endl;}
+void _myShits(double p) { cerr << "my double shits with p is " << p << endl;}
+void _myShits(float p)  { cerr << "my float  shits with p is " << p << endl;}
+BOOST_AUTO_TEST_CASE(apiUsageTest) {
+	EventFramework<int, MyEvents> myab({
+		[(int)MyEvents::EventA]=EventDefinition<int>(_myShits),
+		[(int)MyEvents::EventB]=EventDefinition<int>(_myShits),
+		[(int)MyEvents::EventC]=EventDefinition<int>(_myShits)});
+}
+
+BOOST_AUTO_TEST_SUITE_END();
 
 
 struct IndirectCallSuiteObjects {
@@ -53,7 +134,6 @@ struct IndirectCallSuiteObjects {
 // static initializers
 string IndirectCallSuiteObjects::testOutput = "";
 
-
 static int _r = 1234;
 void indirectlyCallableMethod(int p1) {
 	_r ^= p1;
@@ -63,7 +143,8 @@ BOOST_FIXTURE_TEST_SUITE(IndirectCallSuite, IndirectCallSuiteObjects);
 
 BOOST_AUTO_TEST_CASE(directCallSpikes) {
 	HEAP_MARK();
-	size_t r=19258499592934ul;
+	int r=19258499;
+	_r=1234;
 	for (int p=0; p<numberOfPasses; p++) {
 		unsigned long long start = TimeMeasurements::getMonotonicRealTimeUS();
 		for (unsigned int i=0; i<numberOfCalls; i++) {
@@ -79,7 +160,8 @@ BOOST_AUTO_TEST_CASE(directCallSpikes) {
 
 BOOST_AUTO_TEST_CASE(stdInvokeSpikes) {
 	HEAP_MARK();
-	size_t r=19258499592934ul;
+	int r=19258499;
+	_r=1234;
 	for (int p=0; p<numberOfPasses; p++) {
 		unsigned long long start = TimeMeasurements::getMonotonicRealTimeUS();
 		for (unsigned int i=0; i<numberOfCalls; i++) {
@@ -93,15 +175,16 @@ BOOST_AUTO_TEST_CASE(stdInvokeSpikes) {
 	output("r = " + to_string(r) + "\n");
 }
 
-template<typename _CallableProcedure> struct CallableProcedure {
-	_CallableProcedure&& procedureReference;
-	CallableProcedure(_CallableProcedure&& procedureReference) : procedureReference(procedureReference) {}
-	template<typename _Argument> inline void invoke(_Argument&& argument) {std::invoke(procedureReference, argument);}
+template<typename _Argument> struct CallableProcedure {
+	void(&&procedureReference)(_Argument);
+	CallableProcedure(void(&&procedureReference)(_Argument)) : procedureReference(procedureReference) {}
+	inline void invoke(_Argument& argument) {procedureReference(argument);}
 };
 BOOST_AUTO_TEST_CASE(procedureTemplateSpikes) {
 	HEAP_MARK();
-	size_t r=19258499592934ul;
-	CallableProcedure procedure(indirectlyCallableMethod);
+	int r=19258499;
+	_r=1234;
+	CallableProcedure<int> procedure(indirectlyCallableMethod);
 	class param;
 	for (int p=0; p<numberOfPasses; p++) {
 		unsigned long long start = TimeMeasurements::getMonotonicRealTimeUS();
@@ -119,7 +202,8 @@ BOOST_AUTO_TEST_CASE(procedureTemplateSpikes) {
 BOOST_AUTO_TEST_CASE(stdFunctionSpikes) {
 	HEAP_MARK();
 	std::function<void(int)> callableObject = indirectlyCallableMethod;
-	size_t r=19258499592934ul;
+	int r=19258499;
+	_r=1234;
 	for (int p=0; p<numberOfPasses; p++) {
 		unsigned long long start = TimeMeasurements::getMonotonicRealTimeUS();
 		for (unsigned int i=0; i<numberOfCalls; i++) {
